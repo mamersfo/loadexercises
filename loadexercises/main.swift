ade57a5d81de38371818f5bc3f16ed12836c8128
Martin van Amersfoorth <martin@finalist.nl>
2014-12-27 13:15:35 +0100

    Initial Commit

---
+++ loadexercises/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  loadexercises
+//
+//  Created by Martin van Amersfoorth on 27/12/14.
+//  Copyright (c) 2014 Blijdorp. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
